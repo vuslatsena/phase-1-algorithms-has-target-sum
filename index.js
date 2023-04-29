c7aa739d560453746bc0182065a54306c6d441ef
vuslatsena <vuslatsena23@gmail.com>
2023-04-29 10:18:29 +0300

    Completed Task

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  const complements = new Set();
+
+  for (let i = 0; i < array.length; i++) {
+    if (complements.has(array[i])) {
+      return true;
+    }
+
+    complements.add(target - array[i]);
+  }
+
+  return false;
 }
 
 /* 
   Write the Big O time complexity of your function here
+  My function Big O notion is o(n) because it has a for loop that iterates through the array
 */
 
 /* 
@@ -12,6 +24,12 @@ function hasTargetSum(array, target) {
 
 /*
   Add written explanation of your solution here
+  I create a Set called complements to store the complements of the numbers we've seen so far. 
+  A complement is the number that, when added to the current number, gives us the target value.
+ Then iterate through the array and for each number, we check if its complement is already in the complements Set. 
+ If it is, we know that we have found two numbers that add up to the target, so we return true.
+If the complement is not in the Set, we add the difference between the target and the current number to the Set, so that we can check for it later in the iteration.
+If we reach the end of the loop without finding a pair of numbers that add up to the target, we return false.
 */
 
 // You can run `node index.js` to view these console logs
